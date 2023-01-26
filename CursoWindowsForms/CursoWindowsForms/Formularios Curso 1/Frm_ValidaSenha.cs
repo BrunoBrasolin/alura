9309d252198eb50d25893d13583267f23024f6aa
Bruno Brasolin <bruno.brasolin@t2s.com.br>
2023-01-26 20:20:19 -0300

    Ajustes

---
+++ CursoWindowsForms/CursoWindowsForms/Formularios Curso 1/Frm_ValidaSenha.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CursoWindowsFormsBiblioteca;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaSenha : Form
+    {
+        bool VerSenha = false;
+
+        public Frm_ValidaSenha()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Txt_Senha.Text = String.Empty;
+            Lbl_Resultado.Text = String.Empty;
+        }
+
+        private void Txt_Senha_KeyDown(object sender, KeyEventArgs e)
+        {
+            Cls_Uteis.ChecaForcaSenha verifica = new Cls_Uteis.ChecaForcaSenha();
+
+            Cls_Uteis.ChecaForcaSenha.ForcaDaSenha forca = verifica.GetForcaDaSenha(Txt_Senha.Text);
+
+            Lbl_Resultado.Text = forca.ToString();
+
+            switch (forca)
+            {
+                case Cls_Uteis.ChecaForcaSenha.ForcaDaSenha.Inaceitavel:
+                case Cls_Uteis.ChecaForcaSenha.ForcaDaSenha.Fraca:
+                    Lbl_Resultado.ForeColor = Color.Red;
+                    break;
+                case Cls_Uteis.ChecaForcaSenha.ForcaDaSenha.Aceitavel:
+                    Lbl_Resultado.ForeColor = Color.Blue;
+                    break;
+                case Cls_Uteis.ChecaForcaSenha.ForcaDaSenha.Forte:
+                case Cls_Uteis.ChecaForcaSenha.ForcaDaSenha.Segura:
+                    Lbl_Resultado.ForeColor = Color.Green;
+                    break;
+            }
+
+
+        }
+
+        private void Btn_VerSenha_Click(object sender, EventArgs e)
+        {
+            if (VerSenha)
+            {
+                Txt_Senha.PasswordChar = '*';
+                Btn_VerSenha.Text = "Ver Senha";
+            }
+            else
+            {
+                Txt_Senha.PasswordChar = '\0';
+                Btn_VerSenha.Text = "Esconder Senha";
+            }
+            VerSenha = !VerSenha;
+
+        }
+    }
+}
---
+++ CursoWindowsForms/CursoWindowsForms/Frm_Principal_Menu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Principal_Menu : Form
+    {
+        public Frm_Principal_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void demonstraçãoKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_DemonstracaoKey form = new Frm_DemonstracaoKey();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void helloWorldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_HelloWorld form = new Frm_HelloWorld();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void máscaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Mascara form = new Frm_Mascara();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void validaCPFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF form = new Frm_ValidaCPF();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void validaSenhaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaSenha form = new Frm_ValidaSenha();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Realmente deseja sair?", "Sair aplicação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void cascataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void horizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void verticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+    }
+}
